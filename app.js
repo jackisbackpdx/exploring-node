b798f43de400440a7afcb7f08dc92d458696f373
Jackson <jackson.kelley.g@gmail.com>
2020-04-13 03:02:28 -0700

    added event emitter which raises node events and also used the http module which creates a server that you set to a port with server.listen(3000) or anothe port

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const Logger = 
+const Logger = require('./logger');
+const logger = new Logger;
+
+logger.on('MessageLogged', (arg) => {
+    console.log('Listener Called', arg);
+});
+logger.log('message recieved');
+// log.on('MessageLogged', (args) => {
+//     console.log('Listener called', args);
+// });
--- logger.js
+++ logger.js
@@ -1,10 +1,57 @@
 const os = require('os');
 const path = require('path');
+const fs = require('fs');
+const EventEmitter = require('events');
+const http = require('http');
 
-let filename = path.parse(__filename);
-var totalMem = os.totalmem();
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.write('Hello World');
+        res.end();
+    }
+
+    if(req.url === '/api/courses') {
+        res.write(JSON.stringify([
+            {
+                name: 'Jackson',
+                state: 'Oregon',
+                hobbies: 'Art, Computers, Reading, Music'
+            },
+            {
+                name: 'Jackson',
+                state: 'Oregon',
+                hobbies: 'Art, Computers, Reading, Music'
+            },
+        ]));
+        res.end();
+    }
+});
+
+server.listen(3000);
+console.log('Listening on port 3000');
+
+class Logger extends EventEmitter {
+    log(message) {
+        console.log(message);
+
+        this.emit('MessageLogged', { id: 1, url: 'http://' });
+    }
+}
+
+const totalMem = os.totalmem();
 const freeMem = os.freemem();
+const filename = path.parse(__filename);
 
-console.log(totalMem);
-console.log(freeMem);
-console.log(filename);
+const filesSynchronous = fs.readdirSync('./');
+const filesAsynchronous = fs.readdir('./', (err, files) => {
+    if (err) console.log('Error', err);
+    else console.log('Result', files);
+});
+
+// console.log(`Total Memory: ${totalMem}`);
+// console.log(`Free Memory: ${freeMem}`);
+// console.log(filename);
+// console.log(filesSynchronous);
+// console.log(filesAsynchronous);
+
+module.exports = Logger;
